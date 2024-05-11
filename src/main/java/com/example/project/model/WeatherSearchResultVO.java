8a7889d7e728fcaa943b27cf4866159d21d10cf2
JangSE123 <jse010206@gmail.com>
2024-05-11 18:32:08 +0900

    merge test delete

--- src/main/java/com/example/project/model/WeatherSearchResultVO.java
+++ src/main/java/com/example/project/model/WeatherSearchResultVO.java
@@ -9,7 +9,6 @@ public class WeatherSearchResultVO {
     private String humidity;
     private String precipitation;
     private String windSpeed;
-    private String test;
     private String precipitation_type;
     private String skyCondition;
     private String presentationTime;
